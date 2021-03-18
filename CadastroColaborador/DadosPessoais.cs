2133842d03538dea3320ed02ef062409f8eb5fc3
Alex Almeida <brito.alex@outlook.com>
2021-03-18 18:37:51 -0300

    Praticando aulas de C#, Classes e Atributos

---
+++ CadastroColaborador/DadosPessoais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroColaborador
+{
+    class DadosPessoais
+    {
+        public string nome;
+        public string end;
+        public string tel;
+        public int idade;
+        public double salario;
+    }
+}
---
+++ CadastroColaborador/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroColaborador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosPessoais p1 = new DadosPessoais();
+            
+            Console.WriteLine("***CADASTRO DE COLABORADORES***");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+            Console.Write("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.Write("Endereço: ");
+            p1.end = Console.ReadLine();
+            Console.Write("Telefone: ");
+            p1.tel = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+            Console.Write("Salário: ");
+            p1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("**********************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("DADOS CADASTRADOS COM SUCESSO!");
+            Console.WriteLine();
+            Console.WriteLine("Nome:" + p1.nome);
+            Console.WriteLine("Endereço: " + p1.end);
+            Console.WriteLine("Telefefone: " + p1.tel);
+            Console.WriteLine("Idade: " + p1.idade + " anos");
+            Console.WriteLine("Salário: "  + p1.salario);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("*************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
